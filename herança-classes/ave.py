9c4e01ffc32a6f71cdabfdae3ff0aa2477f4aef7
alebenedet <ale.benedetp@gmail.com>
2023-09-18 22:14:32 -0300

    first commit

---
+++ herança-classes/ave.py
@@ -0,0 +1,26 @@
+from animal import Animal
+from abc import ABC, abstractmethod
+
+
+class Ave(Animal, ABC):
+
+    @abstractmethod
+    def __init__(self, tamanho_passo: int, altura_voo: int):
+        super().__init__(tamanho_passo)
+        self.__altura_voo = altura_voo
+
+    @property
+    def altura_voo(self):
+        return self.__altura_voo
+    
+    @altura_voo.setter
+    def altura_voo(self, altura_voo: int):
+        #if isinstance(altura_voo, int):
+        self.__altura_voo = altura_voo
+
+    def mover(self):
+        return f'{super().mover()} VOANDO'
+
+    def produzir_som(self):
+        return f'AVE: {super().produzir_som()}'
+        
---
+++ herança-classes/gato.py
@@ -0,0 +1,11 @@
+from mamifero import Mamifero
+
+
+class Gato(Mamifero):
+    
+    def __init__(selF):
+        super().__init__(2, 2)
+
+    def miar(self):
+        return f'{super().produzir_som()} SOM: MIAU'
+        
