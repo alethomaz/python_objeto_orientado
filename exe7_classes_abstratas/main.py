82a2dd7284ff671027f4178b01a45f129d810c82
alebenedet <ale.benedetp@gmail.com>
2023-10-20 13:56:53 -0300

    first commit

---
+++ exe7_classes_abstratas/main.py
@@ -0,0 +1,4 @@
+from onibus import Onibus
+
+sao_jose = Onibus(40, 0, False)
+print(sao_jose.ligar())
---
+++ exe7_classes_abstratas/onibus.py
@@ -0,0 +1,25 @@
+from abstractOnibus import AbstractOnibus
+
+
+class Onibus(AbstractOnibus):
+
+    def __init__(self, capacidade: int, total_passageiros: int, ligado: bool):
+        self._capacidade = capacidade
+        self._total_passageiros = total_passageiros
+        self._ligado = ligado
+
+    def ligar(self) -> str:
+        if self.__ligado:
+            raise Exception('Onibus já está ligado')
+        else:
+            self.__ligado = True
+            return 'Onibus ligado'
+        
+    def desligar(self) -> str:
+        if self.__ligado:
+            self.__ligado == False
+            return 'Onibus desligado'
+        else:
+            raise Exception('Onibus já está desligado')
+        
+    
